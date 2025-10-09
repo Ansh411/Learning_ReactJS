476b410b9901d3e1bd8a8f97743861e9e77b0c3e
Ansh411 <4gansh11@gmail.com>
2025-10-09 23:35:40 +0530

    Update

--- Lecture 3 - Laying the Foundation/App.js
+++ Lecture 3 - Laying the Foundation/App.js
@@ -1,17 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div",{id : "parent"},[
-    React.createElement("div", {id : "child"}, [
-        React.createElement("h1",{}, "This is Namaste React 🚀🔥"),
-        React.createElement("h2", {} , "by Akshay Saini"),
-    ]),
-    React.createElement("div",{id : "child2"}, [
-        React.createElement("h1",{}, "I'm an H1 tag"),
-        React.createElement("h2", {}, "I'm an H2 tag"),
-    ]),
-]);
+
+//⭕ React.createElement => (JS Object) => Renders it to HTML DOM
+
+
+//⭕ This is a older method / hard method to create an element but it is very complex and hard to maintain
+
+const heading = React.createElement("h1",
+    {id : "heading"},
+    "Namaste React"
+);
+
+console.log(heading)
+
+/* Now we use JSX to create React Element (JSX stands for Javascript XML)
+
+⭕ JSX has HTML like syntax or XML like syntax 
+
+⭕  JSX element == React.createElement
+
+⭕ JSX => React.createElement => (JS Object) => Renders it to HTML DOM
+
+⭕ JSX is written using "Babel" under Parcel which works as transpiler which transpiled the code to make it React Element then it usual process as it renders to HTML
+(It transpiled before it reaches to JS Engine)
+
+⭕ In JSX, we define class with "className" and other attributes are also defined with "camelCase"
+
+⭕ When we define any JSX element in more than 1 line so we enclose it using parentheses to make sure babel understands it
+
+*/
+
+const jsxHeading = (<h1 className="heading" tabIndex="1">
+    Namaste React from JSX 🚀
+    </h1>)
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
--- Lecture 3 - Laying the Foundation/README.md
+++ Lecture 3 - Laying the Foundation/README.md
@@ -15,3 +15,18 @@ and also we can build dev using "build" in the scripts section
 as to start the server we use : "npm run start" or we can do "npm start"✅
 
 but for building the dev we use : "npm run build" ❌{here we cannot do npm build}
+
+## About JSX (new way of creating react element)
+
+⭕ JSX has HTML like syntax or XML like syntax
+
+⭕ JSX element == React.createElement
+
+⭕ JSX => React.createElement => (JS Object) => Renders it to HTML DOM
+
+⭕ JSX is written using "Babel" under Parcel which works as transpiler which transpiled the code to make it React Element then it usual process as it renders to HTML
+(It transpiled before it reaches to JS Engine)
+
+⭕ In JSX, we define class with "className" and other attributes are also defined with "camelCase"
+
+⭕ When we define any JSX element in more than 1 line so we enclose it using parentheses to make sure babel understands it
