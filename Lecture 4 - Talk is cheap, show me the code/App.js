de23a31c1c163edbc19f067603fda19c16ee977f
Ansh411 <4gansh11@gmail.com>
2025-10-12 08:05:44 +0530

    Adding star icon

--- Lecture 4 - Talk is cheap, show me the code/App.js
+++ Lecture 4 - Talk is cheap, show me the code/App.js
@@ -1776,7 +1776,7 @@ const RestaurantCard = (props) => {
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <span>
-            <h4>{avgRating}⭐</h4>
+            <h4>{avgRating}<i className="fa-solid fa-star"></i></h4>
             <h4>{costForTwo}</h4>
             <h4>{sla.slaString}</h4>
             </span>
